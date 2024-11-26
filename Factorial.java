c4f78c9313cac0282519145f5af4587ff346911d
manogna <komaturimanogna@gmail.com>
2024-11-26 16:12:27 +0530

    changed

--- Factorial.java
+++ Factorial.java
@@ -1,25 +1,15 @@
 public class Factorial {
-    
-    // Recursive function to calculate factorial
-    public static long factorial(int num) {
-        if (num == 0 || num == 1) {
-            return 1;
-        } else {
-            return num * factorial(num - 1);  // Recursively call the function
-        }
-    }
-    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Input: Ask the user to enter a number
-        System.out.print("Enter a number: ");
-        int num = scanner.nextInt();
-        
-        // Calculate and display factorial using the recursive method
-        long result = factorial(num);
-        System.out.println("Factorial of " + num + " is: " + result);
-        
-        scanner.close();
+        int number = 5;  // The number to calculate the factorial of
+        int factorial = 1;
+
+        // Calculate factorial
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+
+        // Print the result
+        System.out.println("Factorial of " + number + " is " + factorial);
     }
 }
+
